80c61ef60eacaee6d6c5df5a80134dbefca7e74d
ze-ubuntu <1079903036@qq.com>
2023-07-06 10:50:42 +0800

    correct select_server question: maxi k

--- tcp_socket/select_server/select_server.c
+++ tcp_socket/select_server/select_server.c
@@ -12,7 +12,7 @@
 int main(  )
 {  
 	int lfd, cfd, cur;
-	int len, i;
+	int len, k;
 	struct sockaddr_in server_addr, client_addr;
 	char client_ip[256];
 	socklen_t client_addr_len = sizeof(client_addr);
@@ -21,13 +21,13 @@ int main(  )
 
 	int opt = 1;
 	setsockopt(lfd, SOL_SOCKET, SO_REUSEADDR, (void *)&opt, sizeof(opt));
-
+	
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(SOCKET_PORT);
 	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	cur = bind(lfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
 
-	cur = listen(lfd, 64);
+	cur = listen(lfd, 128);
 	printf("Waiting client connect ... ....\n");	
 	
 	fd_set rset, allset;
@@ -36,6 +36,14 @@ int main(  )
 	int maxfd = lfd;
 	char buf[1024];
 	int num = 0;
+	
+	int client[1024];
+	for(int i = 0; i < 1024; i++)
+	{
+		client[i] = -1;
+	}
+	int maxi = -1;
+	int sockfd = -1;
 
 	while(1)
 	{
@@ -51,37 +59,63 @@ int main(  )
 			printf("Client IP = %s, PORT = %d\n",
 					inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, client_ip, sizeof(client_ip)),
 					ntohs(client_addr.sin_port));
-			FD_SET(cfd, &allset);
 		
+			for(k = 0; k < 1024; k++)
+			{
+				if(client[k] < 0)
+				{
+					client[k] = cfd;
+					break;
+				}
+			}
+			if(k == 1024)
+			{
+				printf("max error!");
+				return 0;
+			}
+
+			FD_SET(cfd, &allset);
 			if(cfd > maxfd)
 			{
 				maxfd = cfd;
+			}
+
+			if(k > maxi)
+			{
+				maxi = k;
 			}
 			if(num == 1)
 			{
 				continue;
 			}
 		}
-		for(int i = lfd + 1; i <= maxfd; i++)
+		for(int i = 0; i <= maxi; i++)
 		{
-			if(FD_ISSET(i, &rset))
+			if((sockfd = client[i]) < 0)
+				continue;
+			if(FD_ISSET(sockfd, &rset))
 			{
-				len = read(i, buf, sizeof(buf));
+				len = read(sockfd, buf, sizeof(buf));
 				if(len == 0)
 				{
 					printf("close client ...");
-					close(i);
-					FD_CLR(i, &allset);
-					break;
-				}
-				for(int j = 0; j < len; j++)
+					close(sockfd);
+					FD_CLR(sockfd, &allset);
+					client[i] = -1;
+				}else
 				{
-					buf[j] = toupper(buf[j]);
+					for(int j = 0; j < len; j++)
+					{
+						buf[j] = toupper(buf[j]);
+					}
+					write(sockfd, buf, len);
+					write(1, buf, len);
 				}
-				write(i, buf, len);
-				write(1, buf, len);
+				if(num == 1)
+					break;
 			}
 		}
 	}
+	close(lfd);
 	return 0;
 }
