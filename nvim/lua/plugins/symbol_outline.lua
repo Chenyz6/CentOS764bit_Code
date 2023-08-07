0ff8e8091bacd52a57549d9eb2e87ed41b7df9fc
ze-ubuntu <1079903036@qq.com>
2023-08-07 16:23:49 +0800

    2023.08.07

--- nvim/lua/plugins/symbol_outline.lua
+++
@@ -1,4 +0,0 @@
-require("symbols-outline").setup()
-
-
-vim.api.nvim_set_keymap("n", "<leader>so", "<cmd>SymbolsOutline<CR>", {silent = true, noremap = true})
--- nvim/lua/plugins/toggleTerm.lua
+++
@@ -1,6 +0,0 @@
-require('toggleterm').setup({
-  open_mapping = '][',
-  
-  direction = 'float', 
-  shade_terminals = true
-})
